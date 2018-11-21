2a43f36cab0a4b027c2b3456f24249e9bd196263
Rajat <rajat.r@BRILLIO.COM>
2018-11-21 14:06:17 +0530

    Tic Tac Toe Game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tic-Tac-Toe';
+  public status = "Player X's Turn"
+  public state = new Array(9).fill(' ');
+  public xTurn = true;
+  public winner = false;
+  public win = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+  onClick(event, val) {
+    // alert(val);
+    if (!this.winner && this.state[val] === ' ') {
+      this.state[val] = this.xTurn ? "X" : "O";
+      this.checkWinner();
+      if (!this.winner) {
+        this.status = this.xTurn ? "Player O's Turn" : "Player X's Turn";
+      }
+      this.xTurn = !this.xTurn;
+    }
+  }
+  checkWinner() {
+
+    for (let i = 0; i < this.win.length; i++) {
+      let comp = this.win[i];
+      console.log(comp[0]);
+      if (this.state[comp[0]] != ' ' && this.state[comp[0]] === this.state[comp[1]] && this.state[comp[0]] === this.state[comp[2]]) {
+        this.status = this.xTurn ? "Player X Winner" : "Player O winner";
+        this.winner = true;
+        break;
+      }
+
+    }
+  }
+
 }
